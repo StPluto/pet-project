4ee5a25acacc36cee0bd2dad6e9164c358b6ee3b
StPluto <ibavov.ismail@gmail.com>
2023-07-03 01:02:53 +0300

    Started makimg admin panel

---
+++ app/Http/Controllers/AdminPanelController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Posts;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class AdminPanelController extends Controller
+{
+	public function index() : View{
+		return view('admin',['posts'=>Posts::all(),'count'=>Posts::where('id','>','1')->count()] );
+	}
+	public function countRows() {
+     return Posts::where('id','>','1')->count();
+}
+    public function addItem() {
+    	
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MainPageController;
+use App\Http\Controllers\AdminPanelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [MainPageController::class, 'list']);
+Route::get('/admin',[AdminPanelController::class, 'index']);
